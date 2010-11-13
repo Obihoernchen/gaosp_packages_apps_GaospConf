81dc7d617e62db39ab866378775a9f12c0224aa8
Obihörnchen <obihoernchende@gmail.com>
2010-11-13 17:36:38 +0100

    Better fullscreen method for SplashScreen. Added translations: - Romanian - Croatian (Hrvatski) - French - Danish - Italian
    Updated:
    - Russian
    - German
    - English

--- src/com/android/gaospconf/SplashScreen.java
+++ src/com/android/gaospconf/SplashScreen.java
@@ -4,8 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MotionEvent;
-import android.view.Window;
-import android.view.WindowManager;
 
 public class SplashScreen extends Activity {
     protected boolean _active = true;
@@ -14,11 +12,10 @@ public class SplashScreen extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);requestWindowFeature(Window.FEATURE_NO_TITLE);
-              getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-              setContentView(R.layout.splash);
+        super.onCreate(savedInstanceState);
+        	setContentView(R.layout.splash);
        
-        // thread for displaying the SplashScreen
+        // Thread for displaying the SplashScreen
         Thread splashTread = new Thread() {
             @Override
             public void run() {
