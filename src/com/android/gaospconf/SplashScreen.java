6beff31a18181c2096d357873a60487f271dbcf4
Obihörnchen <obihoernchende@gmail.com>
2010-11-13 15:03:01 +0100

    Added a splashscreen :) (because files must be read in and this takes some time)

---
+++ src/com/android/gaospconf/SplashScreen.java
@@ -0,0 +1,52 @@
+package com.android.gaospconf;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashScreen extends Activity {
+    protected boolean _active = true;
+    protected int _splashTime = 3000;
+    
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);requestWindowFeature(Window.FEATURE_NO_TITLE);
+              getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+              setContentView(R.layout.splash);
+       
+        // thread for displaying the SplashScreen
+        Thread splashTread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    int waited = 0;
+                    while(_active && (waited < _splashTime)) {
+                        sleep(100);
+                        if(_active) {
+                            waited += 100;
+                        }
+                    }
+                } catch(InterruptedException e) {
+                    // do nothing
+                } finally {
+                    finish();
+                    startActivity(new Intent("com.android.gaospconf.splashscreen.conf"));
+                    stop();
+                }
+            }
+        };
+        splashTread.start();
+    }
+    
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            _active = false;
+        }
+        return true;
+    }
+}
