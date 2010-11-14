5ff9710e54ef82dcc15092e3bd6c041a7e6d88e8
Obihörnchen <obihoernchende@gmail.com>
2010-11-14 19:18:16 +0100

    Decreased SplashScreen time.

--- src/com/android/gaospconf/SplashScreen.java
+++ src/com/android/gaospconf/SplashScreen.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.os.Handler;
 
 public class SplashScreen extends Activity {
-    protected int _splashTime = 3000;
+    protected int _splashTime = 2000;
     
     /** Called when the activity is first created. */
     @Override
